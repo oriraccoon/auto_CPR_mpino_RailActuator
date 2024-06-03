5305ea0380750d06ea95a3d1af195a6daf8393ea
oriraccoon <djdjskskalal@gmail.com>
2024-06-04 00:11:55 +0900

    sw sensor I2C

--- AutoCPR/Slave1_Mega.ino
+++ AutoCPR/Slave1_Mega.ino
@@ -1,12 +1,20 @@
 #include <Wire.h>
 
-int SensorPin = A0;
 const int buttonPin = 2; // 버튼 핀 번호 설정
+const int sensor1Pin = 3; // 센서 1 핀 번호 설정
+const int sensor2Pin = 4; // 센서 2 핀 번호 설정
+const int sensor3Pin = 5; // 센서 3 핀 번호 설정
+const int sensor4Pin = 6; // 센서 4 핀 번호 설정
+
 volatile bool buttonPressed = false; // 인터럽트에 의해 변경될 변수
 
 void setup() {
   Wire.begin(8); // 슬레이브 주소 8로 I2C 버스 시작
   pinMode(buttonPin, INPUT_PULLUP); // 버튼 핀을 입력으로 설정, 내부 풀업 저항 사용
+  pinMode(sensor1Pin, INPUT); // 센서 1 핀을 입력으로 설정
+  pinMode(sensor2Pin, INPUT); // 센서 2 핀을 입력으로 설정
+  pinMode(sensor3Pin, INPUT); // 센서 3 핀을 입력으로 설정
+  pinMode(sensor4Pin, INPUT); // 센서 4 핀을 입력으로 설정
   attachInterrupt(digitalPinToInterrupt(buttonPin), buttonInterrupt, FALLING); // 버튼 핀에 인터럽트 연결
   Wire.onRequest(requestEvent); // 데이터 요청 시 실행할 함수 등록
 }
@@ -24,9 +32,21 @@ void requestEvent() {
     Wire.write(1); // 버튼이 눌렸다는 신호 전송
     buttonPressed = false; // 상태 초기화
   } else {
-    int sensorValue = analogRead(SensorPin); // 센서에서 데이터 읽기
-    Wire.write(0); // 버튼이 눌리지 않았다는 신호 전송
-    Wire.write(sensorValue >> 8); // 센서 데이터의 상위 8비트 전송
-    Wire.write(sensorValue & 0xFF); // 센서 데이터의 하위 8비트 전송
+    int sensor1Value = digitalRead(sensor1Pin);
+    int sensor2Value = digitalRead(sensor2Pin);
+    int sensor3Value = digitalRead(sensor3Pin);
+    int sensor4Value = digitalRead(sensor4Pin);
+
+    if (sensor1Value == LOW) {
+      Wire.write(2); // 센서 1 인식 신호 전송
+    } else if (sensor2Value == LOW) {
+      Wire.write(3); // 센서 2 인식 신호 전송
+    } else if (sensor3Value == LOW) {
+      Wire.write(4); // 센서 3 인식 신호 전송
+    } else if (sensor4Value == LOW) {
+      Wire.write(5); // 센서 4 인식 신호 전송
+    } else {
+      Wire.write(0); // 변화 없음을 나타내는 신호 전송
+    }
   }
 }
