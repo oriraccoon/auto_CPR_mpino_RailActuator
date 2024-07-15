d6091f318e0146b59ae274ed4435760ab67e177f
oriraccoon <djdjskskalal@gmail.com>
2024-07-15 14:16:36 +0900

    up

--- libraries/RailMotor/src/RailMotor.cpp
+++ libraries/RailMotor/src/RailMotor.cpp
@@ -1,10 +1,20 @@
-#include "RailMotor.h"
+#include <RailMotor.h>
+#include <Arduino.h>
 
 RailMotor::RailMotor(int pul, int dir, int speed)
 {
     _pul = pul;
     _dir = dir;
     _speed = speed;
+}
+
+RailMotor::RailMotor(int pul, int dir, int speed, int limit_for, int limit_back)
+{
+    _pul = pul;
+    _dir = dir;
+    _speed = speed;
+    _limit_for = limit_for;
+    _limit_back = limit_back;
 }
 
 void RailMotor::setup()
@@ -15,18 +25,35 @@ void RailMotor::setup()
 
 void RailMotor::moveForward()
 {
-    digitalWrite(_dir, HIGH);
-    digitalWrite(_pul, HIGH);
-    delayMicroseconds(_speed);
-    digitalWrite(_pul, LOW);
-    delayMicroseconds(_speed);
+    while ( !attached_limit(_limit_for))
+    {
+        digitalWrite(_dir, HIGH);
+        digitalWrite(_pul, HIGH);
+        delayMicroseconds(_speed);
+        digitalWrite(_pul, LOW);
+        delayMicroseconds(_speed);
+    }
 }
 
 void RailMotor::moveBackward()
 {
-    digitalWrite(_dir, LOW);
-    digitalWrite(_pul, HIGH);
-    delayMicroseconds(_speed);
-    digitalWrite(_pul, LOW);
-    delayMicroseconds(_speed);
+    while ( !attached_limit(_limit_back))
+    {
+        digitalWrite(_dir, LOW);
+        digitalWrite(_pul, HIGH);
+        delayMicroseconds(_speed);
+        digitalWrite(_pul, LOW);
+        delayMicroseconds(_speed);
+    }
+}
+
+int RailMotor::attached_limit(int limit)
+{
+    int flag;
+    flag = digitalRead(limit);
+    return flag;
+}
+
+void RailMotor::Reset(){
+    moveBackward();
 }
--- libraries/RailMotor/src/RailMotor.h
+++ libraries/RailMotor/src/RailMotor.h
@@ -6,14 +6,17 @@
 class RailMotor {
   public:
     RailMotor(int pul, int dir, int speed = 10);
-
+    RailMotor(int pul, int dir, int speed = 10, int limit_for, int limit_back);
     void setup();
     void moveForward();
     void moveBackward();
+    int attached_limit(int limit);
+    void Reset();
 
   private:
     int _pul, _dir;
     int _speed;
+    int _limit_for, _limit_back;
 };
 
 #endif
